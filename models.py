6a031a98afac7c849333c2ad706d0fecff3d3a89
Faisal Zaid <zaidalideveloper@gmail.com>
2026-01-16 09:37:27 +0300

    added class users

--- models.py
+++ models.py
@@ -5,3 +5,19 @@ from datetime import datetime
 
 metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
+
+
+
+# -------------------------
+# USERS (KEEP FOR LATER)
+# -------------------------
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text, nullable=False, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    role = db.Column(db.Enum("job_seeker", "employer"), nullable=False)
+
+    serialize_rules = ("-password",)
