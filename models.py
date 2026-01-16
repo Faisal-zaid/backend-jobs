3d0d2ab764dc54fd83bc9239503d42b4b3514bc7
Faisal Zaid <zaidalideveloper@gmail.com>
2026-01-16 09:36:51 +0300

    defined metadata

--- models.py
+++ models.py
@@ -2,3 +2,6 @@ from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from datetime import datetime
+
+metadata = MetaData()
+db = SQLAlchemy(metadata=metadata)
