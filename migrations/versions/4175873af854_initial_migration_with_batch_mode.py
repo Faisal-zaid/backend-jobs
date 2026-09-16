d489d87eea8270d5408ad6793c78721250df415b
faisal ali zaid <zaidalideveloper@gmail.com>
2026-09-16 11:26:05 +0300

    did migrations again

---
+++ migrations/versions/4175873af854_initial_migration_with_batch_mode.py
@@ -0,0 +1,24 @@
+"""Initial migration with batch mode
+
+Revision ID: 4175873af854
+Revises: 
+Create Date: 2026-09-16 11:25:22.779520
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4175873af854'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
